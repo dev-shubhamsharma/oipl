06c060b2be0f68a67972623325adbed40cda78fb
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2022-06-06 14:04:04 +0530

    networking mcq added

---
+++ questions/networking.js
@@ -0,0 +1,104 @@
+const questions = [
+
+    {
+        // 1
+        question: "A collection of hyperlinked documents on the internet forms the",
+        optionA: "WWW",
+        optionB: "E-mail System",
+        optionC: "Mailing List",
+        optionD: "Hypertext Markup Langauge",
+        correctAnswer: "WWW"
+    },
+
+    {
+        // 2
+        question: "The location of a resource on the internet is given by its",
+        optionA: "Protocol",
+        optionB: "URL",
+        optionC: "E-mail address",
+        optionD: "ICQ",
+        correctAnswer: "URL"
+    },
+
+    {
+        // 3
+        question: "The term HTTP stands for :",
+        optionA: "Hyper terminal tracing program",
+        optionB: "Hypertext tracing protoco",
+        optionC: "Hypertext transfer protocol",
+        optionD: "Hypertext transfer program",
+        correctAnswer: "Hypertext transfer protocol"
+    },
+
+    {
+        // 4
+        question: "Which software prevents the external access to a system?",
+        optionA: "Firewall",
+        optionB: "Gateway",
+        optionC: "Router",
+        optionD: "Virus Checker",
+        correctAnswer: "Firewall"
+    },
+
+    {
+        // 5
+        question: "Which one of the following is a valid email address?",
+        optionA: "oipl@gonda.com",
+        optionB: "gmail.com",
+        optionC: "oiplgonda@.com",
+        optionD: "oiplgonda@gmail",
+        correctAnswer: "oipl@gonda.com"
+    },
+
+    {
+        // 6
+        question: "Which one of the following is the most common internet protocol?",
+        optionA: "HTML",
+        optionB: "NetBEUI",
+        optionC: "TCP/IP",
+        optionD: "IPX/SPX",
+        correctAnswer: "TCP/IP"
+    },
+
+    {
+        // 7
+        question: "The term FTP stands for?",
+        optionA: "File transfer program",
+        optionB: "File transmission protocol",
+        optionC: "File transfer protocol",
+        optionD: "File transfer protection",
+        correctAnswer: "File transfer protocol"
+    },
+
+    {
+        // 8
+        question: "Which one of the following is not a network topology?",
+        optionA: "Star",
+        optionB: "Ring",
+        optionC: "Bus",
+        optionD: "Peer to Peer",
+        correctAnswer: "Peer to Peer"
+    },
+
+    {
+        // 9
+        question: "The length of an IPv6 address is?",
+        optionA: "32 bits",
+        optionB: "64 bits",
+        optionC: "128 bits",
+        optionD: "256 bits",
+        correctAnswer: "128 bits"
+    },
+
+    {
+        // 10
+        question: " Which of the following address belongs class A?",
+        optionA: "121.12.12.248",
+        optionB: "130.12.12.248",
+        optionC: "128.12.12.248",
+        optionD: "129.12.12.248",
+        correctAnswer: "121.12.12.248"
+    },
+
+]
+        
