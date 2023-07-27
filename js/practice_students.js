4b23b2a2b07346e33aa0f6df08e3500f29c06c32
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2023-07-27 09:28:56 +0530

    Update practice_students.js

--- js/practice_students.js
+++ js/practice_students.js
@@ -1,5 +1,16 @@
 var students = {
     "Shubham Sharma":"Shubham",
     "Anurag Singh":"Anurag@oipl",
-    "Deepak Tiwari":"Deepak@oipl"
+    "Deepak Tiwari":"Deepak@oipl",
+
+    "Priyanka":"Priyanka@oipl",
+    "Goldi":"Goldi@oipl",
+    "Saloni":"Saloni@oipl",
+    "Ramesh":"Ramesh@oipl",
+    "Vipin":"Vipin@oipl",
+    "Mahfooz":"Mahfooz@oipl",
+    "Mukesh":"Mukesh@oipl",
+    "Rubi":"Rubi@oipl",
+    "Amarjeet":"Amarjeet@oipl",
+    "Pankaj":"Pankaj@oipl"
 };
