ef09201d4dfeb209a6c23836514b7998520ca2c1
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2023-07-28 09:24:41 +0530

    Update exam_students.js

--- js/exam_students.js
+++ js/exam_students.js
@@ -4,6 +4,14 @@ var students = {
     "anurag singh":"Anurag@oipl",
     "deepak tiwari":"Deepak@oipl",
     "anuj mishra":"Anuj@oipl",
-    "satanshu mishra":"Satanshu@oipl"
+    "satanshu mishra":"Satanshu@oipl",
+
+    "Saloni":"Saloni@oipl",
+    "Goldi":"Goldi@oipl",
+    "Ramesh":"Ramesh@oipl",
+    "Amarjeet":"Amarjeet@oipl",
+    "Rubi":"Rubi@oipl",
+    "Pankaj":"Pankaj@oipl"
+    
 
 }
