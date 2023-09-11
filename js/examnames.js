6cec8b38891fed1857febec38d13c6d2cb730e2a
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2023-09-11 13:45:33 +0530

    Update examnames.js

--- js/examnames.js
+++ js/examnames.js
@@ -1,36 +1,47 @@
 const exams = [
     {
-        name :"Chapter 1 - IT Tools",
+        name :"IT Tools & Network",
         totalQuestions : "100",
         timeInMinutes : "70",
         imgUrl : "images/python.png",
-        id :"chapter1_it",
-        filename:"exam_questions/chapter1-it-new.js",
-        questionsAddedInFile:"101"
+        id :"it_test_1",
+        filename:"exam_questions/IT_Set1.js",
+        questionsAddedInFile:"125"
 
     },
+    
+    // {
+    //     name :"Chapter 1 - IT Tools",
+    //     totalQuestions : "100",
+    //     timeInMinutes : "70",
+    //     imgUrl : "images/python.png",
+    //     id :"chapter1_it",
+    //     filename:"exam_questions/chapter1-it-new.js",
+    //     questionsAddedInFile:"101"
 
-    {
-        name :"Chapter 2 - IT Tools",
-        totalQuestions : "60",
-        timeInMinutes : "30",
-        imgUrl : "images/python.png",
-        id :"chapter2_it",
-        filename:"exam_questions/chapter1-it.js",
-        questionsAddedInFile:"82"
+    // },
 
-    },
+    // {
+    //     name :"Chapter 2 - IT Tools",
+    //     totalQuestions : "60",
+    //     timeInMinutes : "30",
+    //     imgUrl : "images/python.png",
+    //     id :"chapter2_it",
+    //     filename:"exam_questions/chapter1-it.js",
+    //     questionsAddedInFile:"82"
 
-    {
-        name :"It Tools and Networks",
-        totalQuestions : "60",
-        timeInMinutes : "30",
-        imgUrl : "images/python.png",
-        id :"it",
-        filename:"exam_questions/it.js",
-        questionsAddedInFile:"60"
+    // },
 
-    },
+    // {
+    //     name :"It Tools and Networks",
+    //     totalQuestions : "60",
+    //     timeInMinutes : "30",
+    //     imgUrl : "images/python.png",
+    //     id :"it",
+    //     filename:"exam_questions/it.js",
+    //     questionsAddedInFile:"60"
+
+    // },
 
     {
         name :"Programming with Python",
