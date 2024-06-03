0862bbceb7aa579dfd54f712cf2f9732c4940c1d
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-06-03 08:21:20 +0530

    Update exam_students.js

--- js/exam_students.js
+++ js/exam_students.js
@@ -8,6 +8,28 @@ var students = {
     "Abhishek":"Abhishek@123",
     "Subhas":"Subhas@123",
     "Aniket":"Aniket@123",
+
+    "Abhinav Dubey":"Abhinav@oipl",
+    "Satyam":"Satyam@oipl",
+    "Ankit Prajapati":"Ankit@oipl",
+    "Vivek Shukla":"Vivek@oipl",
+    "Bhupendra":"Bhupendra@oipl",
+    "Neeraj":"Neeraj@oipl",
+    "Prateek Singh":"Prateek@oipl",
+    "Piyush Gupta":"Piyush@oipl",
+    "Guddu Idrishi":"Guddu@oipl",
+    "Vineet Pandey":"Vineet@oipl",
+    "Himanshu Dubey":"Himanshu@oipl",
+    "Gayatri Gupta":"Gayatri@oipl",
+    "Shalu Mishra":"Shalu@oipl",
+    "Khusi Pandey":"Khusi@oipl",
+    "Jashmeen":"Jashmeen@oipl",
+    "Beauty Shukla":"Beauty@oipl",
+    "Raj Kumar":"Raj@oipl",
+    "Saurabh Kasaudhan":"Saurabh@123",
+    "Anoop Shukla":"Anoop@oipl",
+    "Vivek Sahu":"Vivek@oipl",
+    "Shivam":"Shivam@oipl",
     
     
 
