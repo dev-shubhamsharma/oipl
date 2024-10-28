a7584367a9c6bf3fd05a7ab9ef735f587514af6f
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-10-28 17:32:04 +0530

    Update examnames.js

--- js/examnames.js
+++ js/examnames.js
@@ -1,72 +1,13 @@
 const exams = [
     {
-        name :"IT Tools & Network - Chapter 1",
-        totalQuestions : "100",
-        timeInMinutes : "50",
+        name :"Libreoffice Writer",
+        totalQuestions : "80",
+        timeInMinutes : "40",
         imgUrl : "images/python.png",
-        id :"it_chapter_1",
-        filename:"exam_questions/chapter-1-it.js",
-        questionsAddedInFile:"180"
-
-    },
-    
-    
-    {
-        name :"IT Tools & Network",
-        totalQuestions : "100",
-        timeInMinutes : "70",
-        imgUrl : "images/python.png",
-        id :"it_test_1",
-        filename:"exam_questions/IT_Set1.js",
-        questionsAddedInFile:"200"
-
-    },
-
-    
-    
-    // {
-    //     name :"Chapter 1 - IT Tools",
-    //     totalQuestions : "100",
-    //     timeInMinutes : "70",
-    //     imgUrl : "images/python.png",
-    //     id :"chapter1_it",
-    //     filename:"exam_questions/chapter1-it-new.js",
-    //     questionsAddedInFile:"101"
-
-    // },
-
-    // {
-    //     name :"Chapter 2 - IT Tools",
-    //     totalQuestions : "60",
-    //     timeInMinutes : "30",
-    //     imgUrl : "images/python.png",
-    //     id :"chapter2_it",
-    //     filename:"exam_questions/chapter1-it.js",
-    //     questionsAddedInFile:"82"
-
-    // },
-
-    // {
-    //     name :"It Tools and Networks",
-    //     totalQuestions : "60",
-    //     timeInMinutes : "30",
-    //     imgUrl : "images/python.png",
-    //     id :"it",
-    //     filename:"exam_questions/it.js",
-    //     questionsAddedInFile:"60"
-
-    // },
-
-    {
-        name :"Programming with Python",
-        totalQuestions : "0",
-        timeInMinutes : "0",
-        imgUrl : "images/python.png",
-        id :"python",
-        filename:"python.js",
-        questionsAddedInFile:"0"
+        id :"writer",
+        filename:"exam_questions/writer.js",
+        questionsAddedInFile:"95"
 
     }
-
 
 ]
