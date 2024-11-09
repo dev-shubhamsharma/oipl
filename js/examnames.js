06f7a68880f7f2823b4971f1d080f4dcb98e82d9
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-11-09 14:01:02 +0530

    Update examnames.js

--- js/examnames.js
+++ js/examnames.js
@@ -1,14 +1,14 @@
 const exams = [
-    // {
-    //     name :"Libreoffice Writer",
-    //     totalQuestions : "100",
-    //     timeInMinutes : "40",
-    //     imgUrl : "images/writer.png",
-    //     id :"writer",
-    //     filename:"exam_questions/writer.js",
-    //     questionsAddedInFile:"100"
+    {
+        name :"Libreoffice Writer",
+        totalQuestions : "100",
+        timeInMinutes : "40",
+        imgUrl : "images/writer.png",
+        id :"writer",
+        filename:"exam_questions/writer.js",
+        questionsAddedInFile:"100"
 
-    // },
+    },
 
     {
         name :"Computer Basics and Network",
