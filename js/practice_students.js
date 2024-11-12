9c91e99a03007deaa9b04d920a2c590d93c50ff1
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-11-12 17:15:49 +0530

    Update practice_students.js

--- js/practice_students.js
+++ js/practice_students.js
@@ -1,60 +1,6 @@
 var students = {
-    "Shubham Sharma":"Shubham",
-    "Abhinav Dubey":"Abhinav@oipl",
-    "Satyam":"Satyam@oipl",
-    "Ankit Prajapati":"Ankit@oipl",
-    "Vivek Shukla":"Vivek@oipl",
-    "Bhupendra":"Bhupendra@oipl",
-    "Neeraj":"Neeraj@oipl",
-    "Prateek Singh":"Prateek@oipl",
-    "Piyush Gupta":"Piyush@oipl",
-    "Guddu Idrishi":"Guddu@oipl",
-    "Vineet Pandey":"Vineet@oipl",
-    "Himanshu Dubey":"Himanshu@oipl",
-    "Gayatri Gupta":"Gayatri@oipl",
-    "Shalu Mishra":"Shalu@oipl",
-    "Khusi Pandey":"Khusi@oipl",
-    "Jashmeen":"Jashmeen@oipl",
-    "Beauty Shukla":"Beauty@oipl",
-"Raj Kumar":"Raj@oipl",
-"Saurabh Kasaudhan":"Saurabh@123",
-"Anoop Shukla":"Anoop@oipl",
-"Vivek Sahu":"Vivek@oipl",
-"Shivam":"Shivam@oipl",
-
-    "Pawan":"Pawan@123",
-    "Sameeksha":"Sameeksha@123",
-    "Rubi":"Rubi@123",
-    "Kuldeep":"Kuldeep@123",
-    "Pradeep":"Pradeep@123",
-    "Abhishek":"Abhishek@123",
-    "Subhas":"Subhas@123",
-    "Aniket":"Aniket@123",
+    "Shubham Sharma":"Shubham"
     
     
-
-    "Sudha":"Sudha@123",
-    "Jaya":"Jaya@123",
-    "Ayush":"Ayush@123",
-    "Vaibhav":"Vaibhav@123",
-    "Akarsh":"Akarsh@123",
-    "Saurabh":"Saurabh@123",
-    "Virendra":"Virendra@500",
-    "Suraj":"Suraj@123",
-    "Shivam":"Shivam@123",
-    "Vipul":"Vipul@123",
-    "Aadya":"Aadya@123",
-    "Vinod":"Vinod@123",
-    "Nandini":"Nandini@123",
-    "Nirupam":"Nirupam@123",
-    "Muskan":"Muskan@123",
-    
-    
-    
-    
-    
-   
-
-   
     
 };
