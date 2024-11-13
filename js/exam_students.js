898008523e903720f3da2e5b615932f6605d92f4
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-11-13 10:57:46 +0530

    Update exam_students.js

--- js/exam_students.js
+++ js/exam_students.js
@@ -32,15 +32,8 @@ var students = {
     "Saurabh Chaurasiya":"Saurabh@123",
     "Vivek Paswan":"Vivek@123",
     
-    
-    
-    
-    
     "Mahek Srivastava":"Mahek@123",
     "Vijay Laxmi Shukla":"Laxmi@123",
-    
-    
-    
     
     "Beauty Shukla":"Beauty@123",
     "Anshika Maurya":"Anshika@123",
@@ -62,6 +55,62 @@ var students = {
     "Seema Maurya":"Seema@123",
     "Nidhi Pal":"Nidhi@123",
     "Kumkum Kashyap":"Kumkum@123",
+    
+    "Ankur":"Ankur@oipl",
+    "Ankit Kumar":"Ankit@oipl",
+    "Lakhan Lal":"Lakhan@oipl",
+    "Shiv Baksh":"Shiv@oipl",
+    "Ankit Kumar Verma":"Ankit@oipl",
+    "Tushar Singh":"Tushar@oipl",
+    "Vikas Pandey":"Vikas@oipl",
+    "Shrit Kashyap":"Shrit@oipl",
+    "Piyush giri":"Piyush@oipl",
+    "Awaneesh Maurya":"Awaneesh@oipl",
+    "Kuldeep Yadav":"Awaneesh@oipl",
+    "Budh Ram Yadav":"Budh@oipl",
+    "Anshul Mishra":"Anshul@oipl",
+    "Rajneesh Tiwari":"Rajneesh@oipl",
+    "Noor Alam":"Noor@oipl",
+    "Gangesh Sharma":"Gangesh@oipl",
+    "Rupendra Tiwari":"Rupendra@oipl",
+    "Kafil Ahmad":"Kafil@oipl",
+    "Maneesh Kumar Chauhan":"Maneesh@oipl",
+    "Ayush Kumar Mishra":"Ayush@oipl",
+    "Virendra Singh":"Virendra@oipl",
+    "Satyam Srivastava":"Satyam@oipl",
+    "Ranjeet Sonkar":"Ranjeet@oipl",
+    "Archana Yadav":"Archana@oipl",
+    "Madhu Mishra":"Madhu@oipl",
+    "Laki Pandey":"Laki@oipl",
+    "Afreen":"Afreen@oipl",
+    "Neelu Devi Maurya":"Neelu@oipl",
+    "Shanta":"Shanta@oipl",
+    "Seema Patel":"Seema@oipl",
+    "Nandini Maurya":"Nandini@oipl",
+    "Laxmi Tiwari":"Laxmi@oipl",
+    "Dipali Gupta":"Dipali@oipl",
+    "Manisha Devi":"Manisha@oipl",
+    "Komal":"Komal@oipl",
+    "Renu Devi":"Renu@oipl",
+    "Sankalp Vishwakarma":"Sankalp@oipl",
+    "Ashutosh Tiwari":"Ashutosh@oipl",
+    "Shubham Rajbhar":"Shubham@oipl",
+    "Neha Tiwari":"Neha@oipl",
+    "Khushboo Yadav":"Khushboo@oipl",
+    "Vishal Jaiswal":"Vishal@oipl",
+    "Vibhansh Yadav":"Vibhansh@oipl",
+    "Ashwani Tiwari":"Ashwani@oipl",
+    "Purnima Maurya":"Purnima@oipl",
+    "Anjali Kashyap":"Anjali@oipl",
+    "Aditya":"Aditya@oipl",
+    "Brijesh Tiwari":"Brijesh@oipl",
+    "Fardeen Sidiqqui":"Fardeen@oipl",
+    "Ankit Tiwari":"Ankit@oipl",
+    "Aadarsh Goswami":"Aadarsh@oipl",
+    "Saurabh Singh":"Saurabh@oipl",
+    "Vivek Rao":"Vivek@oipl",
+    "Arti Devi Gupta":"Arti@oipl",
+    "Vaishnavi":"Vaishnavi@oipl",
     
     
 
@@ -162,119 +211,6 @@ var students = {
     "Sushant Sharma":"Sushant@123"
     
     
-    
-    
-    
-    
-
-
-
-    
-
-    // "Suneel Verma":"Suneel@123",
-    // "Pinki Verma":"Pinki@123",
-    // "Ashok Kumar":"Ashok@123",
-    // "Shivam Chaurasiya":"Shivam@123",
-    // "Sachin Kumar":"Sachin@123",
-    // "Karan Prajapati":"Karan@123",
-    // "Amit Sharma":"Amit@123",
-    // "Kalp Nath Yadav":"Kalp@123",
-    // "Suraj Gupta":"Suraj@123",
-    // "Shailendra Kumar":"Shailu@123",
-    // "Raman Kumar":"Raman@123",
-    // "Rekha Yadav":"Rekha@123",
-    // "Anshu Yadav":"Anshu@123",
-    // "Radhika Namdev":"Radhika@123",
-    // "Mahesh":"Mahesh@123",
-    // "Manish Chaurasiya":"Manish@123",
-    
-    // "Nandini Sharma":"Nandini@123",
-    // "Sushmita Verma":"Sushmita@123",
-    // "Shivani Kashyap":"Shivani@123",
-    // "Khusi Kashyap":"Khusi@123",
-    // "Jooli":"Jooli@123",
-    // "Dileep Yadav":"Dileep@123",
-    // "Anand Prakash":"Anand@123",
-    // "Savitri Maurya":"Savitri@123",
-    // "Ranjeet Kumar":"Ranjeet@123",
-    // "Laxmi":"Laxmi@123",
-    
-    // "Urmila":"Urmila@123",
-    // "Sadhna Yadav":"Sadhna@123",
-    // "Aradhana Yadav":"Aradhana@123",
-    // "Mohd Juned":"Juned@123",
-
-
-
-
-
-
-
-
-
-    // "Sameer Verma":"Sameer@123",
-    // "Aashish Kaushal":"Aashish@123",
-    // "Ajay Kumar Choudhary":"Ajay@123",
-    // "Ajay Gautam":"Ajay@123",
-    // "Ajay Maurya":"Ajay@123",
-    // "Dinesh Yadav":"Dinesh@123",
-    // "Archana Maurya":"Archana@123",
-    // "Ashish Kaushal":"Ashish@123",
-    // "Alankrita":"Alankrita@123",
-    // "Adarsh Pandey":"Adarsh@123",
-    // "Aashish Pandey":"Aashish@123",
-    // "Anshika Tiwari":"Anshika@123",
-    // "Archana Singh":"Archana@123",
-    // "Saurabh Mishra":"Saurabh@123",
-    // "Chandan Pathak":"Chandan@123",
-    // "Aditya Mishra":"Aditya@123",
-    // "Nisha Mishra":"Nisha@123"
-        
-    
-
-    
-    // "Shubham Sharma":"Shubham",
-
-    // "Anshika Tiwari":"Anshika@123",
-    // "Archana Singh":"Archana@123",
-    // "Alankrita Srivastava":"Alankrita@123",
-    // "Aadarsh Pandey":"Aadarsh@123",
-    // "Ashish":"Ashish@123",
-    // "Kajal Singh":"Kajal@123",
-    // "Mansi Singh":"Mansi@123",
-    // "Chandan Pathak":"Chandan@123",
-    // "Jyoti Singh":"Jyoti@123",
-
-    
-    // "Anurag":"Anurag@123",
-    // "Manish":"Manish@123",
-    // "Govinda":"Govinda@123",
-
-    // "Gauri Dixit":"Gauri@123",
-    // "Archana Yadav":"Archana@123",
-     // "Aditya Mishra":"Aditya@123",
-    // "Himanshu Gaur":"Himanshu@123",
-    // "Sarvesh Sharma":"Sarvesh@123",
-    // "Ashish Gupta":"Ashish@123",
-    // "Vishal Kumar":"Vishal@123",
-    // "Vinod":"Vinod@123",
-    // "Komal Mishra":"Komal@123",
-    // "Sakshi Rai":"Sakshi@123",
-    // "Pushkar Shukla":"Pushkar@123",
-    // "Kushal Tripathi":"Kushal@123",
-    // "Komal Kashyap":"Komal@123",
-    
-    // "anurag singh":"Anurag@oipl",
-    // "deepak tiwari":"Deepak@oipl",
-    // "anuj mishra":"Anuj@oipl",
-    // "satanshu mishra":"Satanshu@oipl",
-
-    // "Saloni":"Saloni@oipl",
-    // "Goldi":"Goldi@oipl",
-    // "Ramesh":"Ramesh@oipl",
-    // "Amarjeet":"Amarjeet@oipl",
-    // "Rubi":"Rubi@oipl",
-    // "Pankaj":"Pankaj@oipl"
     
 
 }
