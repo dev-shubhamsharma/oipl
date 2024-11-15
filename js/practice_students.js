27954a2f5f64f9f8d71c00cc11c5ed94f943fdb2
Shubham Sharma <42088776+dev-shubhamsharma@users.noreply.github.com>
2024-11-15 16:46:58 +0530

    Update practice_students.js

--- js/practice_students.js
+++ js/practice_students.js
@@ -1,6 +1,11 @@
 var students = {
     "Shubham Sharma":"Shubham",
-    "Manisha Verma":"Manisha@oipl"
+    "Manisha Verma":"Manisha@oipl",
+    "Mayank Jaiswal":"Mayank@oipl",
+    "Vikas Maurya":"Vikas@oipl",
+    "Alok Kumar Yadav":"Alok@oipl",
+    "Alok Pandey":"Alok@oipl",
+    "Sadanand Yadav":"Sadanand@oipl"
     
     
 };
